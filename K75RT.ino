87a36c2293f4239f38010bb6d0a7ed25bcc03661
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2020-02-09 23:36:24 +0100

    Get rid of 2 files shity organisation

--- K75RT.ino
+++ K75RT.ino
@@ -3,15 +3,6 @@ using namespace aunit;
 
 #include "BMW_K75.h"
 
-//BMWK75 (int , int , int , int ) {
-BMWK75 bmw(
-  /*lightSensorPinIn*/ A1,
-  /*lightSwitchPosition1PinIn*/4,
-  /*lightSwitchPosition2PinIn*/ 5,
-  /*lightInPinIn*/ 6,
-  /*oilPressureSensorPin*/A3,
-  /*globalWarningPin*/ 3);
-
 int temperatureSensorPin      = A0;
 int oilPressureSensorPin      = A1;
 int fuelSensorPin             = A2;
@@ -76,11 +67,18 @@ const float VENTILATION_HYSTERESIS = 1000;
 const float WARNING_THRESHOLD_OHMS = 143;
 const float WARNING_HYSTERESIS = 100;
 
-int SHIFT_LIGHT_THERSHOLD_MAX = 8600; //67HZ + 2 Segments
-int SHIFT_LIGHT_THERSHOLD_MIN = 8400; // LA ZONE SUR LE COMPTEUR BMX EST ENTRE 8500 ET 9000
+const int SHIFT_LIGHT_THERSHOLD_MAX = 8600; //67HZ + 2 Segments
+const int SHIFT_LIGHT_THERSHOLD_MIN = 8400; // LA ZONE SUR LE COMPTEUR BMX EST ENTRE 8500 ET 9000
 
-int FUEL_LEVEL_THERSHOLD_MAX = 15; //S'allume en dessous de 10%
-int FUEL_LEVEL_THERSHOLD_MIN = 10; //S'éteint au dessus de 15%
+const int FUEL_LEVEL_THERSHOLD_MAX = 15; //S'allume en dessous de 10%
+const int FUEL_LEVEL_THERSHOLD_MIN = 10; //S'éteint au dessus de 15%
+
+const int LIGHT_SWITH_POSITION_OFF = 0;
+const int LIGHT_SWITH_POSITION_PARKING_LIGHT = 1;
+const int LIGHT_SWITH_POSITION_LOW_BEAM = 2;
+const float LIGHT_SENSOR_THRESHOLD_MIN = 0.33;
+const float LIGHT_SENSOR_THRESHOLD_MAX = 0.66;
+const float OIL_PRESURE_THESHOLD_MIN = 0.33;
 
 boolean fanOn = false;
 
@@ -89,6 +87,16 @@ boolean oilPressureWarning = false;
 boolean gearWarning = false;
 boolean temperatureWarning = false;
 boolean rpmWarning = false;
+
+float mapf(double val, double in_min, double in_max, double out_min, double out_max) {
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+boolean analogToDigital(int value) {
+       if (value < 1023*0.3) return LOW;
+  else if (value > 1023*0.7) return HIGH;
+  else return NULL;
+}
 
 void setup() {
   //Entrées
@@ -110,7 +118,6 @@ void setup() {
   pinMode(gear4Pin, OUTPUT);
   pinMode(gear5Pin, OUTPUT);
 
-  bmw.setup();
 
   for (int i = 1; i <=13; i++) digitalWrite(i, LOW);
   
@@ -137,12 +144,12 @@ void loop() {
  /*********************************
   TIME MANAGEMENT
   *********************************/
-  //If code run over 50 days : Managin time overflow
+  //If code run over 50 days : Manage time overflow
   if(currentMillis < previousMillis) {
     previousMillis = currentMillis;
   }
   
-  //Backupbing before update
+  //Backup before update
   previousMillis = currentMillis;
   
   //update
@@ -151,12 +158,47 @@ void loop() {
   /**********************************
   LIGHTS
   **********************************/
-  bmw.updateLights();
+  bool mUnitLightOutput = false; //TODO : get M-Unit light output
+  if(mUnitLightOutput == false) {
+        //TODO : switch headlight ON
+        //TODO : switch ledring OFF
+      } else {
+        //Light swith & Light sensor value
+        int lightSwitchPosition = 0; //TODO : get switch position
+        int lightSensorValue = 0; //TODO : get lights sensor value
+        
+        if(lightSwitchPosition == LIGHT_SWITH_POSITION_PARKING_LIGHT) {
+            //TODO : switch headlight OFF
+            //TODO : switch ledring ON
+         
+          } else if(lightSwitchPosition == LIGHT_SWITH_POSITION_LOW_BEAM) {
+            //TODO : switch headlight ON
+            //TODO : switch ledring OFF
+         
+          } else if(lightSwitchPosition == LIGHT_SWITH_POSITION_OFF) {
+            if(lightSensorValue >= LIGHT_SENSOR_THRESHOLD_MAX) {
+            //TODO : switch headlight OFF
+            //TODO : switch ledring ON
+              
+            } else if(lightSensorValue <= LIGHT_SENSOR_THRESHOLD_MIN) {
+            //TODO : switch headlight ON
+            //TODO : switch ledring OFF
+
+            }    
+          } else {
+            //ERROR CASE
+            //TODO : switch headlight ON
+            //TODO : switch ledring ON
+ 
+          } 
+        }
 
   /**********************************
   OIL
   **********************************/
-  bmw.getOilPresure();
+  float val = analogRead(oilPressureSensorPin);
+  float oilPressure = mapf(val, 0, 1023,0,1); 
+  oilPressureWarning = oilPressure <= OIL_PRESURE_THESHOLD_MIN ? true: false;
 
  /*********************************
   RPM & SHIFTLIGHT
@@ -188,7 +230,7 @@ void loop() {
   
   //if state changed since last check
   if(currentSpeedState == previousSpeedState) {
-    timeSpentAtPreviousSpeedState += currentMillis - previousMillis;
+    timeSpentAtPreviousSpeedState += (currentMillis - previousMillis);
   } else {
     previousSpeedState = currentSpeedState;
 
@@ -223,10 +265,10 @@ void loop() {
   temperatureSensorValue = analogRead(temperatureSensorPin);
   
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float temperatureSensorVoltage = helper.mapf(temperatureSensorValue, 0, 1023,0,5);
+  float temperatureSensorVoltage = mapf(temperatureSensorValue, 0, 1023,0,5);
 
   //Calcul de la résistance en fonction de la tension mesurée  
-  float temperatureSensorResistance = helper.mapf(temperatureSensorVoltage,0,5,3000,0);// minOhms + ((maxOhms-minOhms) - (temperatureSensorVoltage/5)*(maxOhms-minOhms)) ; //TODO
+  float temperatureSensorResistance = mapf(temperatureSensorVoltage,0,5,3000,0);// minOhms + ((maxOhms-minOhms) - (temperatureSensorVoltage/5)*(maxOhms-minOhms)) ; //TODO
 
   //Conversion de la résistance en température
   //TODO (utile ?)
@@ -253,9 +295,9 @@ void loop() {
   gearBox3Value = analogRead(gearBox3Pin);
 
   //mesure des valeurs des cables de sortie de boite
-  boolean gearBox1Active = helper.analogToDigital(gearBox1Value);
-  boolean gearBox2Active = helper.analogToDigital(gearBox2Value);
-  boolean gearBox3Active = helper.analogToDigital(gearBox3Value);
+  boolean gearBox1Active = analogToDigital(gearBox1Value);
+  boolean gearBox2Active = analogToDigital(gearBox2Value);
+  boolean gearBox3Active = analogToDigital(gearBox3Value);
 
   //conversion en numéro de vitesse
   gearWarning = false;
@@ -280,7 +322,7 @@ void loop() {
   FUEL
   **********************************/
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float fuelSensor = helper.mapf(analogRead(fuelSensorPin), 0, 1023, 0, 100);
+  float fuelSensor = mapf(analogRead(fuelSensorPin), 0, 1023, 0, 100);
 
   if(fuelSensor <= FUEL_LEVEL_THERSHOLD_MIN) {
     digitalWrite(fuelIndicatorPin, HIGH); 
@@ -293,7 +335,6 @@ void loop() {
   **********************************/
 
   globalWarning = oilPressureWarning || gearWarning || temperatureWarning || rpmWarning;
-  bmw.updateWarnings();
 
   digitalWrite(warningPin, globalWarning ? HIGH : LOW); 
 
@@ -304,25 +345,65 @@ void loop() {
   *********************************************/
   if (abs(currentMillis - serialLastSent) >= serialDelay) {
     serialLastSent = currentMillis;
+
+     String text = "";
+
+    //LIGHTS
+      //INPUTS
+      //OUTPUTS
     
-    String text = "";
-    text += "TS: ";
-    text += (float)(temperatureSensorValue);
-    text += "\tVoltage: ";
-    text += (float)(temperatureSensorVoltage);
-    text += "\tΩ: ";
-    text += (float)(temperatureSensorResistance);
-    text += "\tFan/Global Warning: ";
-    text += fanOn ? "X/" : "-/";
-    text += globalWarning ? "X" : "-";
-    text += "\tGear: ";
-    text += (int)(gear);
-    text += "\t";
-    text += (int)(gearBox1Value);
-    text += "/";
-    text += (int)(gearBox2Value);
-    text += "/";
-    text += (int)(gearBox3Value);
+    //OIL
+      //INPUTS
+      //OUTPUTS
+          
+    //RPM & SHIFTLIGHT
+      //INPUTS
+      //OUTPUTS
+          
+    //SPEED
+       //INPUTS
+       //OUTPUTS
+         
+    //TEMPERATURE MOTEUR ET VENTILATION
+    text += "/tTS:/t";
+      //INPUTS
+      text += (float)(temperatureSensorValue);
+      text += "\tVoltage: ";
+      text += (float)(temperatureSensorVoltage);
+      text += "\tΩ: ";
+      text += (float)(temperatureSensorResistance);
+      
+      //OUTPUTS
+      text += "\tState/Warning: ";
+      text += fanOn ? "X/" : "-/";
+      text += temperatureWarning ? "X" : "-";
+      
+    //GEARBOX
+    text += "\tGear:\t";
+      //INPUTS
+      text += (int)(gearBox1Value);
+      text += " ";
+      text += (int)(gearBox2Value);
+      text += " ";
+      text += (int)(gearBox3Value);
+      text += "\t";
+      
+      //OUTPUTS
+      text += (int)(gear);
+    
+          
+    //FUEL
+      //INPUTS
+      //OUTPUTS
+          
+    //ALERTE
+      //INPUTS
+      //OUTPUTS
+      text += globalWarning ? "X" : "-";
+    
+   
+
+
     Serial.begin(9600);
     Serial.println(text);  
     Serial.end();
